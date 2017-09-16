63fb7d05ebba7b4a11cc89cc1fa37392cd633503
Chase Nelson <chase.a.nelson@gmail.com>
2017-09-16 15:31:27 -0400

    wafer page works

---
+++ js/waferSummary.js
@@ -0,0 +1,48 @@
+/*******************************************
+* Calls converts wafer JSON to an array
+*******************************************/
+
+var dbJSON; // JSON object that holds all the values from the database
+var dbArray; // Array made of select values from dbJSON
+var keyArray; // Array to hold the key that matches the dbArray
+function JSONtoArray(response) {
+  dbJSON = JSON.parse(response);
+
+  dbArray = [
+    dbJSON.status,
+    dbJSON.vendor,
+    dbJSON.thickness
+  ];
+
+  keyArray = [
+    'Status',
+    'Vendor',
+    'thickness'
+  ];
+
+  fieldArray = [
+    'status',
+    'vendor',
+    'thickness'
+  ];
+}
+
+function updatePart(id, field, val) {
+  var time = new Date();
+  var h = time.getHours();
+  var m = time.getMinutes();
+  var s = time.getSeconds();
+  var dd = time.getDate();
+  var mm = time.getMonth() + 1;
+  var yyyy = time.getFullYear();
+  if (dd < 10) dd = '0' + dd;
+  if (mm < 10) mm = '0' + mm;
+  time = mm + '-' + dd + '-' + yyyy + ' ' + h + ':' + m + ':' + s;
+  $.ajax({
+    url: 'php/updatePart.php?id=' + id + '&partType=wafer&field=' + field + '&value=' + val,
+  });
+  $.ajax({
+    url: 'php/updatePart.php?id=' + id + '&partType=wafer&field=lastEdit&value=' + time,
+  });
+
+}
---
+++ php/getWaferData.php
@@ -0,0 +1,19 @@
+<?php
+  require_once("database.php");
+  $db = new Database();
+  $id = $_GET['id'];
+
+  $sql = "SELECT * FROM wafer WHERE id=$id";
+  $result = $db->db_query($sql);
+  $result = $result[0];
+
+  $return = new stdClass;
+  $return->name = $result['name'];
+  $return->status = $result['status'];
+  $return->vendor = $result['vendor'];
+  $return->thickness = $result['thickness'];
+  $return->lastEdit = $result['lastEdit'];
+
+  $json = json_encode($return);
+  echo $json;
+?>
