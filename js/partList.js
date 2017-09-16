e5e6f9f1bd574e17e02fa7dbc8ffe97aba801883
Chase Nelson <chase.a.nelson@gmail.com>
2017-09-16 15:43:44 -0400

    fixed error with space in link

--- js/partList.js
+++ js/partList.js
@@ -3,7 +3,7 @@ function showParts(resp) {
 
   var waferHTML = "";
   for (var i = 0; i < data.wafer.length; i++) {
-    waferHTML += "<tr><td><a href=\"waferSummary.html?id= " + data.wafer[i].id + "\">" + data.wafer[i].name + "</a></td></tr>";
+    waferHTML += "<tr><td><a href=\"waferSummary.html?id=" + data.wafer[i].id + "\">" + data.wafer[i].name + "</a></td></tr>";
   }
   if (waferHTML != "" && waferHTML != null) {
     document.getElementById("waferList").innerHTML = waferHTML;
@@ -11,7 +11,7 @@ function showParts(resp) {
 
   var sensorHTML = "";
   for (var i = 0; i < data.sensor.length; i++) {
-    sensorHTML += "<tr><td><a href=\"sensorSummary.html?id= " + data.sensor[i].id + "\">" + data.sensor[i].name + "</a></td></tr>";
+    sensorHTML += "<tr><td><a href=\"sensorSummary.html?id=" + data.sensor[i].id + "\">" + data.sensor[i].name + "</a></td></tr>";
   }
   if (sensorHTML != "" && sensorHTML != null) {
     document.getElementById("sensorList").innerHTML = sensorHTML;
@@ -19,7 +19,7 @@ function showParts(resp) {
 
   var hdiHTML = "";
   for (var i = 0; i < data.hdi.length; i++) {
-    hdiHTML += "<tr><td><a href=\"hdiSummary.html?id= " + data.hdi[i].id + "\">" + data.hdi[i].name + "</a></td></tr>";
+    hdiHTML += "<tr><td><a href=\"hdiSummary.html?id=" + data.hdi[i].id + "\">" + data.hdi[i].name + "</a></td></tr>";
   }
   if (hdiHTML != "" && hdiHTML != null) {
     document.getElementById("hdiList").innerHTML = hdiHTML;
@@ -27,7 +27,7 @@ function showParts(resp) {
 
   var bareModHTML = "";
   for (var i = 0; i < data.bareMode.length; i++) {
-    bareModHTML += "<tr><td><a href=\"bareModSummary.html?id= " + data.bareMod[i].id + "\">" + data.bareMod[i].name + "</a></td></tr>";
+    bareModHTML += "<tr><td><a href=\"bareModSummary.html?id=" + data.bareMod[i].id + "\">" + data.bareMod[i].name + "</a></td></tr>";
   }
   if (bareModHTML != "" && bareModHTML != null) {
     document.getElementById("bareModList").innerHTML = bareModHTML;
@@ -35,7 +35,7 @@ function showParts(resp) {
 
   var assModHTML = "";
   for (var i = 0; i < data.assMod.length; i++) {
-    assModHTML += "<tr><td><a href=\"assModSummary.html?id= " + data.assMod[i].id + "\">" + data.assMod[i].name + "</a></td></tr>";
+    assModHTML += "<tr><td><a href=\"assModSummary.html?id=" + data.assMod[i].id + "\">" + data.assMod[i].name + "</a></td></tr>";
   }
   if (assModHTML != "" && assModHTML != null) {
     document.getElementById("assModList").innerHTML = assModHTML;
