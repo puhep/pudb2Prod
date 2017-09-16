31ab12d5af2edc146e3c3a7eb6ea60d0a26771df
Chase Nelson <chase.a.nelson@gmail.com>
2017-09-16 17:11:33 -0400

    new page links

---
+++ js/createPart.js
@@ -0,0 +1,15 @@
+function openSensor() {
+  window.location = "newSensor.html";
+}
+
+function openHDI() {
+  window.location = "newHDI.html";
+}
+
+function openWafer() {
+  window.location = "newWafer.html";
+}
+
+function openModule() {
+  window.location = "newModule.html";
+}
