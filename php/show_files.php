572d212f1f59efcf5a6ef7d0ff0a3488cb311cc7
Chase Nelson <chase.a.nelson@gmail.com>
2017-09-21 00:03:02 -0400

    properly display files, right directory and html string

--- php/show_files.php
+++ php/show_files.php
@@ -1,9 +1,9 @@
 <?php
   /** Testing to see if you can send get/post vars instead of params with AJAX call */
-  $part_tye = $_GET['partType'];
+  $part_type = $_GET['partType'];
   $part_id  = $_GET['id'];
 
-  $dir = "../phase_2_prod/files/".$part_type."/".$part_id;
+  $dir = "../../phase_2_prod/files/".$part_type."/".$part_id;
   $fileStr = "";
   if(!file_exists($dir)) {
     $fileStr = "<p>No files found</p><br>";
@@ -11,12 +11,12 @@
     if(file_exists($dir) && ($handle = opendir($dir))) {
       while(false !== ($entry=readdir($handle))) {
         if($entry != "." && $entry != "..") {
-          $fileStr = $fileStr + "<a href=\"$dir/$entry\" target=\"_blank\">$entry</a><br>";
+          $fileStr = $fileStr . "<a href=\"$dir/$entry\" target=\"_blank\">$entry</a><br>";
         }
       }
     }
   }
-  $dir = "../phase_2_prod/pics/".$part_type."/".$part_id."/";
+  $dir = "../../phase_2_prod/pics/".$part_type."/".$part_id."/";
   $picStr = "";
   if(!file_exists($dir)) {
     $picStr = "No pictures found <br>";
@@ -25,9 +25,9 @@
     while(false !== ($entry=(readdir($handle)))) {
       if($entry != "." && $entry != ".." && substr($entry,-3) !="txt") {
         $str = rawurlencode($entry);
-        $picStr = $picStr + "<tr><td>";
-        $picStr = $picStr + "<a href=$dir/$str target=\"blank\"> <img src=\"$dir/$entry\" width=\"200\" height=\"200\"></a>";
-        $picStr = $picStr + "</td><td>";
+        $picStr = $picStr . "<tr><td>";
+        $picStr = $picStr . "<a href=$dir/$str target=\"blank\"> <img src=\"$dir/$entry\" width=\"200\" height=\"200\"></a>";
+        $picStr = $picStr . "</td><td>";
         $txt = $dir."/".substr($entry,0,-3)."txt";
         if(file_exists($txt)) {
           $fp = fopen($txt, 'r');
@@ -35,10 +35,10 @@
           fclose($fp);
         }
         #echo "picture text here";
-        $picStr += "</td></tr>";
+        $picStr = $picStr . "</td></tr>";
       }
     }
-    $picStr += "</table>";
+    $picStr = $picStr . "</table>";
   }
   $return = new stdClass;
   $return->picStr  = $picStr;
