a38c788c739836eb6de90998a9f9e44da9383c3c
Chase Nelson <chase.a.nelson@gmail.com>
2017-09-21 00:02:00 -0400

    modified some functions

--- php/functions.php
+++ php/functions.php
@@ -3,64 +3,73 @@ require_once("database.php");
 
 ### displays a list of files associated with a part as downloadable links
 ### generalizable to any part type
-function show_files($part_type, $part_id){
-  $dir = "../phase_2_prod_prod/files/".$part_type."/".$part_id;
+function show_files(/*$part_type, $part_id*/) {
+  /** Testing to see if you can send get/post vars instead of params with AJAX call */
+  $part_tye = $_GET['partType'];
+  $part_id  = $_GET['id'];
+  echo $part_tye;
+
+  $dir = "../phase_2_prod/files/".$part_type."/".$part_id;
+  $HTMLstr = "Hello";
   if(!file_exists($dir)) {
-  	echo "No files found <br>";
+  	$HTMLstr = "<p>No files found</p><br>";
   } else {
     if(file_exists($dir) && ($handle = opendir($dir))) {
       while(false !== ($entry=readdir($handle))) {
         if($entry != "." && $entry != "..") {
-          echo "<a href=\"$dir/$entry\" target=\"_blank\">$entry</a>";
-          echo "<br>";
+          $HTMLstr = $HTMLstr + "<a href=\"$dir/$entry\" target=\"_blank\">$entry</a><br>";
         }
       }
     }
   }
+  $return = new stdClass;
+  $return->htmlStr = $HTMLstr;
+  $json = json_encode($return);
+  echo $json;
 }
 
 ### show all pictures with their associated comments for a part in a table
 ### generalizable for any part type
-function show_pictures($part_type, $part_id){
-    $dir = "../phase_2_prod_prod/pics/".$part_type."/".$part_id."/";
-    if(!file_exists($dir)){
-		  echo "No pictures found <br>";
-		  return;
-    }
-    if(file_exists($dir) && ($handle = opendir($dir))){
-        echo "<table border=1>";
-        while(false !== ($entry=(readdir($handle)))){
-            if($entry != "." && $entry != ".." && substr($entry,-3) !="txt"){
-                $str = rawurlencode($entry);
-                echo "<tr>";
-                echo "<td>";
-                echo "<a href=$dir/$str target=\"blank\"> <img src=\"$dir/$entry\" width=\"200\" height=\"200\"></a>";
-                echo "</td>";
-                echo "<td>";
-                $txt = $dir."/".substr($entry,0,-3)."txt";
-                if(file_exists($txt)){
-                    $fp = fopen($txt, 'r');
-                    echo nl2br(fread($fp, filesize($txt)));
-                    fclose($fp);
-                }
-                #echo "picture text here";
-                echo "</td>";
-                echo "</tr>";
-            }
+function show_pictures($part_type, $part_id) {
+  $dir = "../phase_2_prod/pics/".$part_type."/".$part_id."/";
+  $HTMLstr = "";
+  if(!file_exists($dir)) {
+    $HTMLstr = "No pictures found <br>";
+  } elseif (file_exists($dir) && ($handle = opendir($dir))) {
+    $HTMLstr = "<table border=1>";
+    while(false !== ($entry=(readdir($handle)))) {
+      if($entry != "." && $entry != ".." && substr($entry,-3) !="txt") {
+        $str = rawurlencode($entry);
+        $HTMLstr += "<tr><td>";
+        $HTMLstr += "<a href=$dir/$str target=\"blank\"> <img src=\"$dir/$entry\" width=\"200\" height=\"200\"></a>";
+        $HTMLstr += "</td><td>";
+        $txt = $dir."/".substr($entry,0,-3)."txt";
+        if(file_exists($txt)) {
+          $fp = fopen($txt, 'r');
+          $HTMLstr += nl2br(fread($fp, filesize($txt)));
+          fclose($fp);
         }
-        echo "</table>";
+        #echo "picture text here";
+        $HTMLstr += "</td></tr>";
+      }
     }
+    $HTMLstr += "</table>";
+  }
+  $return = new stdClass;
+  $return->htmlStr = $HTMLstr;
+  $json = json_encode($return);
+  echo $json;
 }
 ### show the sensors for a test in a table. On the edit page, make the fields
 ### editable with placeholders for the current values
-function show_sensors($data, $edit=0, $type="sensor"){
-    if($edit==0){
+function show_sensors($data, $edit=0, $type="sensor") {
+    if($edit==0) {
         echo "<table border=1 cellpadding=5>";
         echo "<th>".ucfirst($type)."</th>";
         echo "<th>X (cm)</th>";
         echo "<th>Y (cm)</th>";
-        if($type=="sensor"){ echo "<th>Channel</th>"; }
-        foreach($data as $row){
+        if($type=="sensor") { echo "<th>Channel</th>"; }
+        foreach($data as $row) {
             echo "<tr>";
             echo "<td>";
             echo $row[$type."Name"];
@@ -71,7 +80,7 @@ function show_sensors($data, $edit=0, $type="sensor"){
             echo "<td>";
             echo $row[$type.'YPos'];
             echo "</td>";
-            if($type=="sensor"){
+            if($type=="sensor") {
                 echo "<td>";
                 echo $row['sensorChannel'];
                 echo "</td>"; }
@@ -85,8 +94,8 @@ function show_sensors($data, $edit=0, $type="sensor"){
         echo "<th>X (cm)</th>";
         echo "<th>Y (cm)</th>";
         $i=0;
-        if($type=="sensor"){ echo "<th>Channel</th>"; }
-        foreach($data as $row){
+        if($type=="sensor") { echo "<th>Channel</th>"; }
+        foreach($data as $row) {
             echo "<tr>";
             echo "<td>";
             echo $row[$type.'Name'];
@@ -97,13 +106,13 @@ function show_sensors($data, $edit=0, $type="sensor"){
             echo "<td>";
             echo "<input placeholder=\"".$row[$type.'YPos']."\" name=\"".$type."YPos[$i]\" type=\"number\" step=\"0.1\" >";
             echo "</td>";
-            if($type=="sensor"){
+            if($type=="sensor") {
                 echo "<td>";
-                if($row['sensorChannel'] == "" and $row['curChannel'] != ""){ $row['sensorChannel'] = $row['curChannel']." (Default)"; }
+                if($row['sensorChannel'] == "" and $row['curChannel'] != "") { $row['sensorChannel'] = $row['curChannel']." (Default)"; }
                 echo "<input placeholder=\"".$row['sensorChannel']."\" name=\"channel[$i]\" type=\"number\" min=\"100\" step=\"1\" >";
                 echo "</td>";
                 echo "</tr>"; }
-            if($type=="sensor"){ $nameType = "thermal"; }
+            if($type=="sensor") { $nameType = "thermal"; }
             else{ $nameType = $type; }
             echo "<input type='hidden' name=\"".$nameType."_id[$i]\" value='".$row[$type.'ID']."'>";
             $i++;
@@ -113,7 +122,7 @@ function show_sensors($data, $edit=0, $type="sensor"){
 }
 
 function addTempVsTimeFile($type, $id, $file) {
-  $directory = "../phase_2_prod_prod/files/$type/$id/";
+  $directory = "../phase_2_prod/files/$type/$id/";
   $targetFile = $directory.$file['name'];
   $fileType = pathinfo($targetFile,PATHINFO_EXTENSION);
   if($fileType != "csv") {
@@ -131,7 +140,7 @@ function addTempVsTimeFile($type, $id, $file) {
 }
 
 function addSheetThicknessContour($id, $file) {
-  $directory = "../phase_2_prod_prod/files/sheet/$id/";
+  $directory = "../phase_2_prod/files/sheet/$id/";
   if (!file_exists($directory)) {
     mkdir($directory);
     chmod($directory, 0777);
@@ -148,7 +157,7 @@ function addSheetThicknessContour($id, $file) {
 }
 
 function addSheetBowContour($id, $file) {
-  $directory = "../phase_2_prod_prod/files/sheet/$id/";
+  $directory = "../phase_2_prod/files/sheet/$id/";
   if (!file_exists($directory)) {
     mkdir($directory);
     chmod($directory, 0777);
@@ -165,32 +174,32 @@ function addSheetBowContour($id, $file) {
 }
 
 function add_file($type,$id,$files) {
-  $targetDir = "../phase_2_prod_prod/files/$type/$id/";
+  $targetDir = "../../phase_2_prod/files/$type/$id/";
   ### if the directory for the structure does not exist, create it and make it editable
-  if (!file_exists($targetDir)) {
+    if (!file_exists($targetDir)) {
     mkdir($targetDir);
     chmod($targetDir,0777);
   }
   for ($i = 0; $i < sizeof($files['name']); $i++) {
     $targetFile = $targetDir.$files['name'][$i];
     if (!move_uploaded_file($files['tmp_name'][$i], $targetFile)) { // If file fails to upload send error message
-      echo "<h2>Sorry, an error has occurred. Try again or bother Greg & Chase until they help</h2><br>";
+      echo "<br><h2>Sorry, an error has occurred.</h2><br>";
     }
   }
 }
 
-function add_pic($type,$id,$files,$notes){
+function add_pic($type,$id,$files,$notes) {
     $picupload=1;
-    $targetdir = "../phase_2_prod_prod/pics/$type/$id/";
+    $targetdir = "../phase_2_prod/pics/$type/$id/";
     $targetfile = $targetdir.$files['pic']['name'];
     $imageFileType = pathinfo($targetfile,PATHINFO_EXTENSION);
     ### if the directory for the test does not exist, create it and make it editable
-    if(!file_exists($targetdir)){
+    if(!file_exists($targetdir)) {
       mkdir($targetdir);
       chmod($targetdir,0777);
 	  }
     ### don't allow duplicate uploads
-    if(file_exists($targetfile)){
+    if(file_exists($targetfile)) {
       echo "Sorry, file already exists.<br>".$backmessage;
       $picupload = 0;
     }
@@ -200,7 +209,7 @@ function add_pic($type,$id,$files,$notes){
       $picupload = 0;
     }
     ### if none of the errors have been detected, proceed with the upload
-    if($picupload==1){
+    if($picupload==1) {
     #echo "ok to upload";
     move_uploaded_file($files['pic']['tmp_name'], $targetfile);
     $fp = fopen(substr($targetfile,0,-3)."txt","w");
@@ -213,17 +222,17 @@ function add_pic($type,$id,$files,$notes){
 
 ### shorthand to make some of the other pages a little more readable
 ### if the query returns only one line, it can be accessed with $data[0]
-#function db_query($sql,$db){
+#function db_query($sql,$db) {
 #    $db->query($sql);
 #    $i=0;
-#    while($db->nextRecord()){
+#    while($db->nextRecord()) {
 #        $data[$i]=$db->Record;
 #        $i++;
 #    }
 #    return $data;
 #}
 
-function testData2($testID,$db){
+function testData2($testID,$db) {
     $sql = "SELECT t.name as testName, t.id as testID, t.coolant_temp as coolantTemp,
 ss.id as ssID, ss.name as ssName
 FROM test t
@@ -233,7 +242,7 @@ WHERE t.id=".$testID;
     return $data[0];
 }
 
-function sensorTestData($testID,$db){
+function sensorTestData($testID,$db) {
     $sql = "SELECT s.id as sensorID, s.name as sensorName, s.cur_channel as curChannel,
 st.xpos as sensorXPos, st.ypos as sensorYPos, st.channel as sensorChannel
 FROM test t
@@ -244,7 +253,7 @@ WHERE t.id=".$testID;
     return $sensorData;
 }
 
-function heaterTestData($testID,$db){
+function heaterTestData($testID,$db) {
     $sql = "SELECT h.id as heaterID, h.name as heaterName,
 ht.xpos as heaterXPos, ht.ypos as heaterYPos
 FROM test t
@@ -255,7 +264,7 @@ WHERE t.id=".$testID;
     return $heaterData;
 }
 
-function moduleTestData($testID,$db){
+function moduleTestData($testID,$db) {
     $sql = "SELECT m.id as moduleID, m.name as moduleName,
 mt.xpos as moduleXPos, mt.ypos as moduleYPos
 FROM test t
