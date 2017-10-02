ed44d2854cb18c6c7b6e5ddb34cb2000afc596f3
Chase Nelson <chase.a.nelson@gmail.com>
2017-10-02 10:57:10 -0400

    added misc parts

--- js/partList.js
+++ js/partList.js
@@ -2,42 +2,64 @@ function showParts(resp) {
   data = JSON.parse(resp);
 
   var waferHTML = "";
-  for (var i = 0; i < data.wafer.length; i++) {
-    waferHTML += "<tr><td><a href=\"waferSummary.html?id=" + data.wafer[i].id + "\">" + data.wafer[i].name + "</a></td></tr>";
+  if (data.wafer !== null) {
+    for (var i = 0; i < data.wafer.length; i++) {
+      waferHTML += "<tr><td><a href=\"waferSummary.html?id=" + data.wafer[i].id + "\">" + data.wafer[i].name + "</a></td></tr>";
+    }
   }
-  if (waferHTML != "" && waferHTML != null) {
+  if (waferHTML !== "" && waferHTML !== null) {
     document.getElementById("waferList").innerHTML = waferHTML;
   }
 
   var sensorHTML = "";
-  for (var i = 0; i < data.sensor.length; i++) {
-    sensorHTML += "<tr><td><a href=\"sensorSummary.html?id=" + data.sensor[i].id + "\">" + data.sensor[i].name + "</a></td></tr>";
+  if (data.sensor !== null) {
+    for (var i = 0; i < data.sensor.length; i++) {
+      sensorHTML += "<tr><td><a href=\"sensorSummary.html?id=" + data.sensor[i].id + "\">" + data.sensor[i].name + "</a></td></tr>";
+    }
   }
-  if (sensorHTML != "" && sensorHTML != null) {
+  if (sensorHTML !== "" && sensorHTML !== null) {
     document.getElementById("sensorList").innerHTML = sensorHTML;
   }
 
   var hdiHTML = "";
-  for (var i = 0; i < data.hdi.length; i++) {
-    hdiHTML += "<tr><td><a href=\"hdiSummary.html?id=" + data.hdi[i].id + "\">" + data.hdi[i].name + "</a></td></tr>";
+  if (data.hdi !== null) {
+    for (var i = 0; i < data.hdi.length; i++) {
+      hdiHTML += "<tr><td><a href=\"hdiSummary.html?id=" + data.hdi[i].id + "\">" + data.hdi[i].name + "</a></td></tr>";
+    }
   }
-  if (hdiHTML != "" && hdiHTML != null) {
+  if (hdiHTML !== "" && hdiHTML !== null) {
     document.getElementById("hdiList").innerHTML = hdiHTML;
   }
 
   var bareModHTML = "";
-  for (var i = 0; i < data.bareMod.length; i++) {
-    bareModHTML += "<tr><td><a href=\"moduleSummary.html?id=" + data.bareMod[i].id + "\">" + data.bareMod[i].name + "</a></td></tr>";
+  if (data.bareMod !== null) {
+    for (var i = 0; i < data.bareMod.length; i++) {
+      bareModHTML += "<tr><td><a href=\"moduleSummary.html?id=" + data.bareMod[i].id + "\">" + data.bareMod[i].name + "</a></td></tr>";
+    }
   }
-  if (bareModHTML != "" && bareModHTML != null) {
+  if (bareModHTML !== "" && bareModHTML !== null) {
     document.getElementById("bareModList").innerHTML = bareModHTML;
   }
 
   var assModHTML = "";
-  for (var i = 0; i < data.assMod.length; i++) {
-    assModHTML += "<tr><td><a href=\"moduleSummary.html?id=" + data.assMod[i].id + "\">" + data.assMod[i].name + "</a></td></tr>";
+  if (data.assMod !== null) {
+    for (var i = 0; i < data.assMod.length; i++) {
+      assModHTML += "<tr><td><a href=\"moduleSummary.html?id=" + data.assMod[i].id + "\">" + data.assMod[i].name + "</a></td></tr>";
+    }
   }
-  if (assModHTML != "" && assModHTML != null) {
+  if (assModHTML !== "" && assModHTML !== null) {
     document.getElementById("assModList").innerHTML = assModHTML;
   }
+
+  var miscPartHTML = "";
+  if (data.miscPart !== null) {
+    for (var i = 0; i < data.miscPart.length; i++) {
+      miscPartHTML += "<tr><td><a href=\"miscPartSummary.html?id=" + data.miscPart[i].id + "\">" + data.miscPart[i] + "</a></td></tr>";
+    }
+  }
+  if (miscPartHTML !== "" && miscPartHTML !== null) {
+    document.getElementById('miscPartList').innerHTML = miscPartHTML;
+  }
+  if (data.miscPart !== "hello") console.log('hello');
+  if (data.miscPart == null) console.log('world');
 }
--- php/partList.php
+++ php/partList.php
@@ -1,18 +1,20 @@
 <?php
   require_once 'database.php';
   $db = new Database();
-  $wafer   = $db->db_query("SELECT id, name FROM wafer");
-  $sensor  = $db->db_query("SELECT id, name FROM sensor");
-  $hdi     = $db->db_query("SELECT id, name FROM hdi");
-  $bareMod = $db->db_query("SELECT id, name FROM module WHERE isAssembled=\"false\"");
-  $assMod  = $db->db_query("SELECT id, name FROM module WHERE isAssembled=\"true\"");
+  $wafer    = $db->db_query("SELECT id, name FROM wafer");
+  $sensor   = $db->db_query("SELECT id, name FROM sensor");
+  $hdi      = $db->db_query("SELECT id, name FROM hdi");
+  $bareMod  = $db->db_query("SELECT id, name FROM module WHERE isAssembled=\"false\"");
+  $assMod   = $db->db_query("SELECT id, name FROM module WHERE isAssembled=\"true\"");
+  $miscPart = $db->db_query("SELECT id, name FROM miscPart");
 
   $return = new stdClass;
-  $return->wafer   = $wafer;
-  $return->sensor  = $sensor;
-  $return->hdi     = $hdi;
-  $return->bareMod = $bareMod;
-  $return->assMod  = $assMod;
+  $return->wafer    = $wafer;
+  $return->sensor   = $sensor;
+  $return->hdi      = $hdi;
+  $return->bareMod  = $bareMod;
+  $return->assMod   = $assMod;
+  $return->miscPart = $miscPart;
   $json = json_encode($return);
   echo $json;
 ?>
