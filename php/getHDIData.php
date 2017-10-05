970698ed44e33a3e3951ca3aaac24a6977958804
Chase Nelson <chase.a.nelson@gmail.com>
2017-10-05 11:33:18 -0400

    notes work (#2)

--- php/getHDIData.php
+++ php/getHDIData.php
@@ -10,12 +10,16 @@
   $sql    = "SELECT id, name FROM module WHERE fromHDI =$id";
   $module = $db->db_query($sql);
 
+  $sql   = "SELECT notetext FROM notes WHERE part_id=$id and part_type=\"hdi\"";
+  $notes = $db->db_query($sql);
+
   $return = new stdClass;
   $return->name     = $result['name'];
   $return->status   = $result['status'];
   $return->location = $result['location'];
   $return->lastEdit = $result['lastEdit'];
   $return->module   = $module[0];
+  $return->notes    = $notes;
 
   $json = json_encode($return);
   echo $json;
